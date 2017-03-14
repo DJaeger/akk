102936186078654b8ab19f04bb4d9fe9c0f1e08e
Daniel Jäger <daniel-jaeger@online.de>
2017-03-14 15:34:54 +0100

    Always show the menu

--- inc/menu.php
+++ inc/menu.php
@@ -3,15 +3,13 @@ echo "<ul>\n";
 href("start", $id, "li", "");
 href("statistik", $id, "li", "");
 href("anonstat", $id, "li", "");
-if ($id != "start") {
-    href("printakk",$id,"li","");
-    if ($info->akkrolle == 9) href("mneu",$id,"li","");
-    if ($info->akkrolle == 9) href("einnahmen",$id,"li","");
-    if ($info->akkrolle == 9) href("aenderungen",$id,"li","");
-    if ($info->akkrolle == 9) href("user",$id,"li","");
-    if ($info->akkrolle == 9) href("upload",$id,"li","");
-    href("passwd",$id,"li","");
-}
+href("printakk",$id,"li","");
+if ($info->akkrolle == 9) href("mneu",$id,"li","");
+if ($info->akkrolle == 9) href("einnahmen",$id,"li","");
+if ($info->akkrolle == 9) href("aenderungen",$id,"li","");
+if ($info->akkrolle == 9) href("user",$id,"li","");
+if ($info->akkrolle == 9) href("upload",$id,"li","");
+href("passwd",$id,"li","");
 href("logout",$id,"li","");
 echo "</ul>\n";
 ?>
