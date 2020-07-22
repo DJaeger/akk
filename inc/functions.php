0f770529aa73069eee534453e03fd40ae9513250
Daniel Jäger <daniel-jaeger@online.de>
2020-07-23 01:00:49 +0200

    Added web installer

--- inc/functions.php
+++ inc/functions.php
@@ -114,7 +114,7 @@ function fuzzystring($nstring) {
         if ($s != $s0) {$fuzzystring = $fuzzystring . $s;}
         $s0 = $s;
     }
-    return $fuzzystring; 
+    return $fuzzystring;
 }
 
 function zformat($zahl) {
@@ -192,33 +192,33 @@ function selected($base,$name,$option) {
 }
 
 function csv_to_array($filename='', $delimiter=',', $rowcount=false) {
-	if(!file_exists($filename) || !is_readable($filename))
-		return FALSE;
+    if(!file_exists($filename) || !is_readable($filename))
+        return FALSE;
 
-	$header = NULL;
-	$data = array();
-	if (($handle = fopen($filename, 'r')) !== FALSE)
-	{
-		while (($row = fgetcsv($handle, 0, $delimiter, '"')) !== FALSE)
-		{
-			$num = count($row);
-			if(!$header) {
-				if ($rowcount && $num != $rowcount) {
-					return false;
-				}
-				foreach($row AS $value) {
-					$header[] = trim($value,'﻿"');
-				}
-			} else {
-				if ($rowcount) {
-					$row = array_slice($row, 0, $rowcount);
-				}
-				$data[] = array_combine($header, $row);
-			}
-		}
-		fclose($handle);
-	}
-	return $data;
+    $header = NULL;
+    $data = array();
+    if (($handle = fopen($filename, 'r')) !== FALSE)
+    {
+        while (($row = fgetcsv($handle, 0, $delimiter, '"')) !== FALSE)
+        {
+            $num = count($row);
+            if(!$header) {
+                if ($rowcount && $num != $rowcount) {
+                    return false;
+                }
+                foreach($row AS $value) {
+                    $header[] = trim($value,'﻿"');
+                }
+            } else {
+                if ($rowcount) {
+                    $row = array_slice($row, 0, $rowcount);
+                }
+                $data[] = array_combine($header, $row);
+            }
+        }
+        fclose($handle);
+    }
+    return $data;
 }
 
 function arr2ini(array $a, array $parent = array()) {
@@ -241,22 +241,126 @@ function arr2ini(array $a, array $parent = array()) {
 }
 
 function placeholders($text, $count=0, $separator=",") {
-	$result = array();
-	if($count > 0){
-		for($x=0; $x<$count; $x++){
-			$result[] = $text;
-		}
-	}
+    $result = array();
+    if($count > 0){
+        for($x=0; $x<$count; $x++){
+            $result[] = $text;
+        }
+    }
 
-	return implode($separator, $result);
+    return implode($separator, $result);
 }
 
 function parms($string,$data) {
-	$indexed=$data==array_values($data);
-	foreach($data as $k=>$v) {
-		if(is_string($v)) $v="'$v'";
-		if($indexed) $string=preg_replace('/\?/',$v,$string,1);
-		else $string=str_replace(":$k",$v,$string);
-	}
-	return $string;
+    $indexed=$data==array_values($data);
+    foreach($data as $k=>$v) {
+        if(is_string($v)) $v="'$v'";
+        if($indexed) $string=preg_replace('/\?/',$v,$string,1);
+        else $string=str_replace(":$k",$v,$string);
+    }
+    return $string;
+}
+
+function recreateTables($db) {
+    $sql ="SET FOREIGN_KEY_CHECKS=0;";
+    $db->exec($sql);
+    $sql ="DROP TABLE IF EXISTS tblakk, tbladress, tblbeitrag, tblpay, tbluser;";
+    $db->exec($sql);
+    $sql ="SET FOREIGN_KEY_CHECKS=1;";
+    $db->exec($sql);
+    print("Dropped old tables.\n<br />");
+
+    $sql ="CREATE TABLE tblakk (
+          akkID int(10) unsigned NOT NULL auto_increment,
+          mitgliedsnummer int(10) unsigned default NULL,
+          refcode varchar(15) default NULL,
+          vorname varchar(60) default NULL,
+          nachname varchar(60) default NULL,
+          strasse varchar(64) default NULL,
+          plz varchar(10) default NULL,
+          ort varchar(40) default NULL,
+          nation varchar(2) default NULL,
+          lv varchar(10) default NULL,
+          kv varchar(50) default NULL,
+          geburtsdatum date default NULL,
+          stimmberechtigung tinyint(3) default NULL,
+          offenerbeitrag int(10) unsigned default NULL,
+          eintrittsdatum date default NULL,
+          schwebend tinyint(3) default NULL,
+          suchname varchar(120) default NULL,
+          suchvname varchar(120) default NULL,
+          akk tinyint(3) unsigned NOT NULL default '0',
+          akkrediteur varchar(50) default NULL,
+          geaendert datetime default NULL,
+          kommentar varchar(255) NOT NULL,
+          warnung varchar(1) default NULL,
+          offenerbeitragold int(10) unsigned default NULL,
+          PRIMARY KEY  (akkID),
+          KEY ix_mnr (mitgliedsnummer)
+        );";
+    $db->exec($sql);
+    print("Created table 'tblakk'.\n<br />");
+
+    $sql ="CREATE TABLE tbladress (
+          adressID int(10) unsigned NOT NULL auto_increment,
+          akkID int(10) unsigned NOT NULL,
+          mitgliedsnummer int(10) unsigned default NULL,
+          vorname varchar(250) default NULL,
+          nachname varchar(60) default NULL,
+          strasse varchar(64) default NULL,
+          plz varchar(10) default NULL,
+          ort varchar(40) default NULL,
+          lv varchar(10) default NULL,
+          kv varchar(50) default NULL,
+          akkrediteur varchar(50) default NULL,
+          geaendert datetime default NULL,
+          kommentar varchar(255) default NULL,
+          edit tinyint(3) unsigned default NULL,
+          new tinyint(3) unsigned default NULL,
+          PRIMARY KEY  (adressID),
+          KEY tbladress_ibfk_1 (akkID),
+          CONSTRAINT tbladress_ibfk_1 FOREIGN KEY (akkID) REFERENCES tblakk (akkID)
+        );";
+    $db->exec($sql);
+    print("Created table 'tbladress'.\n<br />");
+
+    $sql ="CREATE TABLE tblbeitrag (
+          mnr varchar(20) NOT NULL,
+          opjahr int(11) NOT NULL,
+          beitragsoll int(11) NOT NULL,
+          beitragist int(11) default NULL,
+          datumsoll date default NULL,
+          datumist date default NULL,
+          bemerkung varchar(255) default NULL
+        );";
+    $db->exec($sql);
+    print("Created table 'tblbeitrag'.\n<br />");
+
+    $sql ="CREATE TABLE tblpay (
+          beitragID int(10) unsigned NOT NULL auto_increment,
+          akkID int(10) unsigned default NULL,
+          mitgliedsnummer varchar(20) default NULL,
+          beitragoffen int(11) default NULL,
+          gezahlt int(11) default NULL,
+          akkrediteur varchar(50) default NULL,
+          geaendert datetime default NULL,
+          kommentar varchar(255) default NULL,
+          PRIMARY KEY  (beitragID),
+          KEY akkID (akkID),
+          CONSTRAINT tblpay_ibfk_1 FOREIGN KEY (akkID) REFERENCES tblakk (akkID)
+        );";
+    $db->exec($sql);
+    print("Created table 'tblpay'.\n<br />");
+
+    $sql ="CREATE TABLE tbluser (
+          login varchar(20),
+          name  varchar(60),
+          rolle int
+        );";
+    $db->exec($sql);
+    print("Created table 'tbluser'.\n<br />");
+
+    $sql ="CREATE UNIQUE INDEX iuser01 ON tbluser (name);";
+    $db->exec($sql);
+    print("Created index on 'tbluser'.\n<br />");
 }
---
+++ install.php
@@ -0,0 +1,249 @@
+<?php
+$id="install";
+ini_set('include_path', 'inc');
+include("db.php");
+include("functions.php");
+include_once("passwdfkt.php");
+
+header("Content-Type: text/html; charset=utf-8");
+
+?><!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="viewpOrt" content="width=device-width, initial-scale=1">
+        <title>Akk Installation</title>
+        <link rel="shOrtcut icon" href="/favicon.ico" >
+        <link rel="stylesheet" type="text/css" href="/css/akk.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="/css/print.css" media="print">
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap-orange-diff.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="/css/bootstrap-akk.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="/css/font-awesome.min.css" media="screen" />
+    </head>
+    <body>
+        <div class="navbar navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand logo" href="https://piraten-tools.de/">
+                        <img src="/img/piratentools-logo.png">
+                    </a>
+                    <a class="navbar-brand" href="/">Akk</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="container">
+<?php
+if ( !empty($_POST) ) {
+?>
+                <div class="text-center">
+<?php
+    $config['database'] = $_POST['database'];
+	$config['database']['driver'] = 'mysql';
+    $config['akk'] = $_POST['akk'];
+    $config['system']['rootdir'] = realpath(dirname(__FILE__));
+    $config_ini = arr2ini($config);
+    file_put_contents('inc/akk.ini', $config_ini);
+	$info = new allginfo('akk.ini',1);
+
+    try {
+        $db = new mydb();
+
+		if ( $db->query("SHOW TABLES LIKE 'tbluser'")->rowCount() == 0 ) {
+			recreateTables($db);
+		}
+
+		if ( $db->query("SELECT login FROM tbluser")->rowCount() == 0 ) {
+			$sql ="INSERT INTO tbluser (login,name,rolle) VALUES (':adminname','Administrator',9);";
+			$rs = $db->prepare($sql);
+			$rs->bindParam(':adminname', $_POST['admin']['username'], PDO::PARAM_INT);
+			$rs->execute();
+			print("Added " . $_POST['admin']['username'] . " to 'tbluser'.\n<br />");
+
+			$User=$_POST['admin']['username'];
+			$Passwort=$_POST['admin']['password'];
+			pSetPasswd($User,$Passwort);
+            print("Added " . $_POST['admin']['username'] . " to htpasswd.\n<br />");
+        }
+?>
+                    <h1>Wohoo</h1>
+                    <h2>We are ready</h2>
+                    <h3>Let's start</h3>
+                    <h4 id="timer"></h4>
+
+					<script>
+						var count=5;
+						var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+						function timer() {
+							count=count-1;
+							if (count < 0) {
+								clearInterval(counter);
+								//counter ended, do something here
+								window.location = "/";
+								return;
+							}
+							//Do code for showing the number of seconds here
+							document.getElementById("timer").innerHTML=count;
+						}
+					</script>
+<?php
+    } catch (\Throwable $t) {
+        echo "Konnte keine Datenbank-Verbindung herstellen";
+    }
+?>
+                </div>
+<?php
+} else {
+?>
+                <h2>Installation</h2>
+                <form action="" method="post" class="form-horizontal">
+                    <fieldset>
+
+                        <legend>Datenbank</legend>
+
+                        <div class="form-group">
+                            <label class="col-sm-2 control-label" for="database[driver]">Treiber:</label>
+                            <div class="col-sm-10">
+                                <select name='database[driver]' id='database_driver' class="form-control" disabled>
+<?php
+    $availableDrivers = PDO::getAvailableDrivers();
+    foreach ($availableDrivers AS $driver) {
+?>                                <option <?=selected("database","driver",$driver);?>><?=$driver;?></option>
+<?php
+    }
+?>
+								</select>
+								<span id="helpBlock" class="help-block">
+									Momentan wird nur mysql unterstützt.
+								</span>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="database[host]">Host:</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="text" name="database[host]" value="<?=@$_POST['database']['host'];?>" placeholder="localhost">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="database[db]">Datenbank:</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="text" name="database[db]" value="<?=@$_POST['database']['db'];?>" placeholder="akk">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="database[username]">Username:</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="text" name="database[username]" value="<?=@$_POST['database']['username'];?>" placeholder="akk">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="database[password]">Password:</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="password" name="database[password]" value="<?=@$_POST['database']['password'];?>" placeholder="">
+							</div>
+						</div>
+
+					</fieldset>
+					<fieldset>
+
+						<legend>Akkreditierung</legend>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="akk[Veranstaltung]">Veranstaltung:</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="text" name="akk[Veranstaltung]" value="<?=@$_POST['akk']['Veranstaltung'];?>" placeholder="">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="akk[startdate]">Startdatum:</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="date" name="akk[startdate]" value="<?=@$_POST['akk']['startdate'];?>" placeholder="dd.mm.yyyy">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="akk[enddate">Enddatum:</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="date" name="akk[enddate]" value="<?=@$_POST['akk']['enddate'];?>" placeholder="dd.mm.yyyy">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="akk[Ort]">Ort:</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="text" name="akk[Ort]" value="<?=@$_POST['akk']['Ort'];?>" placeholder="Berlin">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="akk[Ebene]">Ebene:</label>
+							<div class="col-sm-10">
+								<select name='akk[Ebene]' id='database_Ebene' class='form-control'>
+									<option <?=selected("akk","Ebene","BV");?> value='BV'>Bund</option>
+									<option <?=selected("akk","Ebene","LV");?> value='LV'>Land</option>
+									<option <?=selected("akk","Ebene","KV");?> value='KV'>Kreis</option>
+								</select>
+							</div>
+						</div>
+
+						<div class="form-group" id="akk_av_wrapper">
+							<div class="col-sm-offset-2 col-sm-10">
+								<div class="checkbox">
+									<label>
+										<input type="checkbox" name="akk[AV]" value="1" <?=checked("akk","AV","1");?> />
+										Aufstellungsversammlung?
+									</label>
+								</div>
+							</div>
+						</div>
+
+					</fieldset>
+					<fieldset>
+
+						<legend>Administrator</legend>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="admin[username]">Benutzername:</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="text" name="admin[username]" value="<?=@$_POST['admin']['username'];?>" placeholder="">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label class="col-sm-2 control-label" for="admin[password]">Passwort:</label>
+							<div class="col-sm-10">
+								<input class="form-control" type="password" name="admin[password]" value="<?=@$_POST['admin']['password'];?>" placeholder="">
+							</div>
+						</div>
+
+					</fieldset>
+
+					<div class="form-group">
+						<div class="col-sm-offset-2 col-sm-10">
+							<button type="submit" class="btn btn-primary">Einrichten</button>
+						</div>
+					</div>
+
+                </form>
+<?php
+}
+?>
+                <script src="/js/jquery.min.js"></script>
+                <script src="/js/bootstrap.min.js" ></script>
+            </div><!-- /.container -->
+        </div><!-- /.content -->
+    </body>
+</html>
